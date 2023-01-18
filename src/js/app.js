0c18bfe11f175b25f2ed21e61d7514c9e40cf002
Petar Ganchev <PetarGanchev@users.noreply.github.com>
2023-01-18 12:19:21 +0000

    add function

--- src/js/app.js
+++ src/js/app.js
@@ -7,4 +7,28 @@ window.addEventListener("DOMContentLoaded", () => {
   button.addEventListener("click", () => {
     alert("💣");
   });
+
+      const main = document.querySelector(".main");
+      const image = document.createElement('div');
+      image.classList.add('image');
+        //img
+        const img = document.createElement('img');
+        img.src = "https://www.w3schools.com/howto/img_avatar.png";
+        
+        //images
+        image.appendChild(img);
+
+        //main
+        main.appendChild(image)
+
+//scaleImage
+function scaleImage() {
+  image.style.transform = "scale(2)";
+}
+
+      image.addEventListener('click', scaleImage);
+
+
+      console.log(main, image, img);
+
 });
